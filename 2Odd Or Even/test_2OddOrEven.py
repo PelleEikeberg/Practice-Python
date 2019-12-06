d5f6f16c9d3fc97e5c9f597b9f5413207c631e2d
pellee <pellee@student.matnat.uio.no>
2019-12-06 16:28:13 +0100

    fixed name of .py does not start with number making it easy to import

---
+++ 2Odd Or Even/test_2OddOrEven.py
@@ -0,0 +1,29 @@
+from _2OddOrEven import Number
+
+def test_numbergetsaccsepted():
+    number = float(1)
+
+    succsess = True
+
+    try:
+        x = Number(number)
+        y = Number(number=2)
+    except:
+        succsess = False
+    
+    assert succsess == True, "the program did not accept a float, might have error in __init__"
+
+
+def test_2isEven():
+    number = 2
+    x = Number(2)
+    succsess = x.isEven()
+    assert succsess, "2 is not an even number, Runtime error in Number.isEven()"
+
+
+
+
+if __name__ == "__main__":
+
+    test_numbergetsaccsepted()
+    test_2isEven()
